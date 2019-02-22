0c28806a2bc746e059e629c0aabf03220a2654c0
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-02-21 22:15:48 -0800

    boilerplating: wires up an action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,18 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {fetchPosts} from "../actions";
 
-export default class PostList extends Component {
+class PostList extends Component {
+  componentDidMount = props => {
+    this.props.fetchPosts();
+  };
+
   render() {
     return <div>Post List</div>;
   }
 }
+
+export default connect(
+  null,
+  {fetchPosts}
+)(PostList);
