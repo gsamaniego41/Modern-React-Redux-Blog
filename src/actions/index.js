764ff15ff379ca937fe4f0633d235c358ff418d0
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-02-22 21:14:24 -0800

    thunk - action creator returns a function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  // Bad approach - breaks a Redux rule
-  const response = await jsonPlaceholder.get("/posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: response
+export const fetchPosts = () => {
+  return function(dispatch, getState) {
+    const response = jsonPlaceholder.get("/posts");
+
+    return {
+      type: "FETCH_POSTS",
+      payload: response
+    };
   };
 };
