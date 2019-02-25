2c3b6732ceb2559c161342c35f0c3e573ad5c73a
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-02-25 08:01:00 -0800

    finds unique userIds

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // _.map goes through all the different posts and will pull off only the userId
+  // _.uniq will return an array with just the unique userIds
+
+  // iterate over userIds - and for every unique id, call fetchUser()
+  userIds.forEach(id => dispatch(fetchUser(id)));
+  // async await doesn't work with forEach
 };
 
 export const fetchPosts = () => async dispatch => {
